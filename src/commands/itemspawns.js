d065bb29addb5fb79a6997218812a43f45831ce4
dginovker <dcress01@uoguelph.ca>
2020-09-08 12:03:21 -0400

    Better fix (for real this time) also a new development branch so I can do this without pushing to master

--- src/commands/itemspawns.js
+++ src/commands/itemspawns.js
@@ -19,12 +19,10 @@ module.exports = {
     item_id = isNaN(item_id) ? 1 : +item_id;
     page = isNaN(page) ? 1 : +page;
 
-    // const results = await connection_server.query('SELECT item_id, name, loc_data AS spawns FROM `ground_spawns` LEFT JOIN `item_configs` ON item_configs.id = ground_spawns.item_id WHERE `item_id` = ?', [item_id]).catch(error);
-    let results = await alasql
+    const results = await alasql
     .promise([
       `SELECT item_id, item_configs.name, loc_data AS spawns FROM json('${liveserver_configs_dir}/ground_spawns.json') AS ground_spawns OUTER JOIN json('${liveserver_configs_dir}/item_configs') AS item_configs ON item_configs.id = ground_spawns.item_id WHERE ground_spawns.item_id = "${item_id}"`,
-    ]);
-    results = results[0];
+    ][0]);
 
     if (!results.length)
       return msg.channel.send('No item found with specified ID, or item has no spawns.');
