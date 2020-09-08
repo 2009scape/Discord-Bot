982702dcc64886eff01da48789db94d02329ee81
dginovker <dcress01@uoguelph.ca>
2020-09-08 12:48:25 -0400

    Move npcdrops to alasql

--- src/commands/npcdrops.js
+++ src/commands/npcdrops.js
@@ -1,6 +1,8 @@
-const { error } = require("../helpers/logging.js");
 const { tablePages, postPages } = require("../helpers/functions.js");
-const { connection_server } = require('../database.js');
+const alasql = require('alasql');
+const {
+  liveserver_configs_dir,
+} = require("../config.json");
 
 module.exports = {
   name        : 'npcdrops',
@@ -17,7 +19,11 @@ module.exports = {
     npc_id = isNaN(npc_id) ? 1 : +npc_id;
     page = isNaN(page) ? 1 : +page;
 
-    const results = await connection_server.query("SELECT npc_id, name, CONCAT_WS('~', `default`, `main`, `charm`) AS drops FROM `npc_drops` LEFT JOIN `npc_configs` ON npc_configs.id = npc_drops.npc_id WHERE `npc_id` = ?", [npc_id]).catch(error);
+    //const results = await connection_server.query("SELECT npc_id, name, CONCAT_WS('~', `default`, `main`, `charm`) AS drops FROM `npc_drops` LEFT JOIN `npc_configs` ON npc_configs.id = npc_drops.npc_id WHERE `npc_id` = ?", [npc_id]).catch(error);
+    const results = await alasql
+    .promise([
+      `SELECT npc_configs.npc_id, npc_configs.name, CONCAT_WS('~', drop_tables.default, drop_tables.main, drop_tables.charm) AS drops FROM json('${liveserver_configs_dir}/drop_tables.json') AS drop_tables LEFT JOIN json('${liveserver_configs_dir}/npc_configs.json') AS npc_configs ON npc_configs.id = drop_tables.npc_id WHERE drop_tables.npc_id = ? "${npc_id}"`,
+    ][0]);
 
     if (!results.length)
       return msg.channel.send('No npc found with specified ID, or npc has no drops.');
@@ -26,7 +32,11 @@ module.exports = {
 
     drops = drops.filter(d=>!isNaN(d[0]) && d[0] > 0);
 
-    const items = await connection_server.query(`SELECT id, name FROM \`item_configs\` WHERE id IN(${drops.map(d=>d[0]).join(',')})`);
+    //const items = (`SELECT id, name FROM \`item_configs\` WHERE id IN(${drops.map(d=>d[0]).join(',')})`);
+    const items = await alasql
+    .promise([
+      `SELECT item_configs.id, item_configs.name FROM json('${liveserver_configs_dir}/item_configs.json') AS item_configs WHERE id IN(${drops.map(d=>d[0]).join(',')})`,
+    ][0]);
 
     drops = drops.map(drop=>[
       drop[0],
