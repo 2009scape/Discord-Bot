1036717a99c18ad974f48fd78e6d1b29092924d0
dginovker <dcress01@uoguelph.ca>
2020-09-09 15:58:29 -0400

    fix npcid

--- src/commands/npcid.js
+++ src/commands/npcid.js
@@ -1,37 +1,45 @@
 const { tablePages, postPages } = require("../helpers/functions.js");
-const alasql = require('alasql');
-const fs = require('fs');
-const {
-  liveserver_configs_dir,
-} = require("../config.json");
+const alasql = require("alasql");
+const { liveserver_configs_dir } = require("../config.json");
 
 module.exports = {
-  name        : 'npcid',
-  aliases     : ['npc_id'],
-  description : 'Get an npc ID from the database based on their name',
-  args        : ['name', 'page?'],
-  guildOnly   : true,
-  cooldown    : 3,
-  botperms    : ['SEND_MESSAGES'],
-  userperms   : [],
-  execute     : async (msg, args) => {
-    let [,npc_name, page] = args.join(' ').match(/([\sA-Z]+)(\d*)/i).map(m=>m.trim());
+  name: "npcid",
+  aliases: ["npc_id"],
+  description: "Get an npc ID from the database based on their name",
+  args: ["name", "page?"],
+  guildOnly: true,
+  cooldown: 3,
+  botperms: ["SEND_MESSAGES"],
+  userperms: [],
+  execute: async (msg, args) => {
+    let [, npc_name, page] = args
+      .join(" ")
+      .match(/([\sA-Z]+)(\d*)/i)
+      .map((m) => m.trim());
 
     page = isNaN(page) ? 1 : +page;
 
-    let results = alasql(`SELECT id, name, examine, lifepoints FROM json('${liveserver_configs_dir}/npc_configs.json') WHERE name LIKE "%${npc_name}%"`);
+    let results = await alasql.promise(
+      [
+        `SELECT id, name, examine, lifepoints FROM json('${liveserver_configs_dir}/npc_configs.json') WHERE name LIKE "%${npc_name}%"`,
+      ][0]
+    );
 
     if (!results.length)
-      return msg.channel.send('No npc found with a similar name.');
+      return msg.channel.send("No npc found with a similar name.");
 
-    results = results.map(npc=>[
+    results = results.map((npc) => [
       npc.id,
-      `${npc.lifepoints.toString().padEnd(4, ' ')}♥`,
+      `${npc.lifepoints.toString().padEnd(4, " ")}♥`,
       npc.name.toLowerCase(),
-      (npc.examine || '').toLowerCase().replace(/'/g, ''),
+      (npc.examine || "").toLowerCase().replace(/'/g, ""),
     ]);
 
-    const pages = await tablePages(['ID', 'HP', 'Name', 'Examine'], results, `__***Npc with names similar to \`${npc_name}\`:***__`);
+    const pages = await tablePages(
+      ["ID", "HP", "Name", "Examine"],
+      results,
+      `__***Npc with names similar to \`${npc_name}\`:***__`
+    );
 
     postPages(msg, pages, page);
   },
